202f8b2670c961dc06513d53f75d8d98f9c9f7db
Manal taha <mnalth544@gmail.com>
2025-07-18 22:44:05 +0300

    fresh start

---
+++ lib/view/pages/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget{
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
