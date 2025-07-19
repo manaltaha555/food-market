1a546e2c04d2462ec1e8395f9cbae60529e3111a
Manal taha <mnalth544@gmail.com>
2025-07-19 13:03:56 +0300

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_market/view/pages/onboarding_page.dart';
 import 'package:food_market/view/pages/splash_screen.dart';
 
 void main() {
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen(),
+      home: OnBoarding(),
     );
   }
 }
---
+++ lib/view/pages/onboarding_page.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  PageController pageController = PageController();
+  int selectedIndex = 0;
+  List content = [
+    {
+      "title": "Hello World\nHello World",
+      "body":
+          "Now eat well, Now eat well, Now eat well, Now eat well, Now eat well, Now eat well, Now eat well,",
+    },
+    {
+      "title": "Select the\nFavorities Menu",
+      "body":
+          "don't leave the house, don't leave the house, don't leave the house, don't leave the house, don't leave the house, ",
+    },
+    {
+      "title": "Select the\nFavorities Menu",
+      "body":
+          "Now eat well, don't leave the house,You can choose your favorite food only with one click",
+    },
+    {
+      "title": "Good food at a cheap price",
+      "body": "You can eat at expensive restaurants with affordable price",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/Pattern.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            Expanded(
+              child: Image.asset(
+                "assets/images/${selectedIndex == 4 ? "onboarding2" : "onboarding"}.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: PageView.builder(
+                controller: pageController,
+                physics: NeverScrollableScrollPhysics(),
+                onPageChanged: (value) => setState(() {
+                  selectedIndex = value;
+                }),
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 120),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          content[index]["title"],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(content[index]["body"], textAlign: TextAlign.center),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(child: 
+                selectedIndex == 0
+                    ? Text("Skip")
+                    : IconButton(
+                        icon: Icon(Icons.arrow_back_rounded),
+                        onPressed: () {
+                          pageController.previousPage(
+                            duration: Duration(seconds: 1),
+                            curve: Curves.linear,
+                          );
+                        },
+                      ),
+                ),
+                Expanded(
+                  child: SmoothPageIndicator(
+                    controller: pageController,
+                    count: content.length,
+                    effect: const WormEffect(
+                      dotColor: Colors.grey,
+                      activeDotColor: Color(0xffD61355),
+                      dotHeight: 16,
+                      dotWidth: 16,
+                      type: WormType.thinUnderground,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      pageController.nextPage(
+                        duration: Duration(seconds: 1),
+                        curve: Curves.linear,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
